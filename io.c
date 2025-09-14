622878a6eb3e66846d80b828cd18d73ea80deb12
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-09-14 18:04:22 +0200

    Add a functioning? write_int, refactor other functions

--- io.c
+++ io.c
@@ -11,9 +11,7 @@ int
 read_char() {
   char buf[1];
   size_t nbytes = sizeof(buf);
-  ssize_t bytes_read;
-  int fd = 0;
-  bytes_read = read(fd, buf, nbytes);
+  ssize_t bytes_read = read(STDIN_FILENO, buf, nbytes);
 
   //Think this is wrong, it should return EOF without throwing an error?
   if (bytes_read > 0) {
@@ -25,12 +23,8 @@ read_char() {
 /* Writes c to stdout.  If no errors occur, it returns 0, otherwise EOF */
 int
 write_char(char c) {
-
   size_t nbytes = sizeof(c);
-  ssize_t bytes_write;
-  int fd = 1;
-
-  bytes_write = write(fd, &c , nbytes);
+  ssize_t bytes_write = write(STDOUT_FILENO, &c , nbytes);
 
   if (bytes_write) {
     return 0;
@@ -42,11 +36,9 @@ write_char(char c) {
 int
 write_string(char* s) {
   size_t nbytes = strlen(s);
-  ssize_t bytes_write;
-  int fd = 1;
 
-  bytes_write = write(fd,s,nbytes);
-  bytes_write = write(fd, "\0", sizeof("\0"));
+  ssize_t bytes_write = write(STDOUT_FILENO, s, nbytes);
+
   if (bytes_write) {
     return 0;
   }
@@ -58,5 +50,13 @@ write_string(char* s) {
  */
 int
 write_int(int n) {
+  char convertedInt = n + '0';
+  char* pCI = &convertedInt;
+  size_t nbytes = sizeof(convertedInt);
+  ssize_t bytes_write = write(STDOUT_FILENO,  pCI, nbytes);
+
+  if (bytes_write) {
+    return 0;
+  }
   return EOF;
 }
