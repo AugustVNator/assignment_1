fb1fa96416779bd46f2813632e88fc0aa12e6f74
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-09-17 15:55:56 +0200

    Add two tests

--- test.sh
+++ test.sh
@@ -8,3 +8,12 @@ out="0,3,5;"
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
 
+in="abccbaabcq"
+out="5;"
+
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
+
+in="bbbaabbaq"
+out="3,4,7"
+
+[[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
