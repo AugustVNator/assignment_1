c79612884d80efbe92eefd994e7d0074ef08a999
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-09-17 08:45:37 +0200

    Change stack to linked list

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "io.h"
 
 #include <unistd.h>
@@ -11,65 +13,129 @@
  * interpreter as  specified in the handout.
  */
 
-typedef struct {
-    int *data;
-    int size;
-    int capacity;
-} Stack;
+struct Node {
+    int data;
+    struct Node *next;
+} Node;
 
-static void stack_init(Stack *stack){
-    //Måske skal data initialiseres på en anden måde
-    stack->data = NULL;
-    stack->size = 0;
-    stack->capacity = 1;
+// static void stack_init(Stack *stack){
+//     //Måske skal data initialiseres på en anden måde
+//     stack->data = NULL;
+//     stack->size = 0;
+//     stack->capacity = 1;
+// }
+//
+// static void stack_push(Stack *stack) {
+//     if (stack->size == stack->capacity) {
+//         stack->capacity *= 2;
+//     }
+// }
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
-static void stack_push(Stack *stack) {
-    if (stack->size == stack->capacity) {
-        stack->capacity *= 2;
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void deleteFromEnd(struct Node** head) {
+    if (*head == NULL) {
+        return;
+    }
+    struct Node* temp = *head;
+    if (temp->next == NULL) {
+        free(temp);
+        *head = NULL;
+        return;
+    }
+    while (temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next = NULL;
+}
+
+void printLinkedList(struct Node* head) {
+    struct Node* temp = head;
+    if (temp->next == NULL) {
+        write_int(temp->data);
+        return;
+    }
+    while (temp != NULL) {
+        write_int(temp->data);
+        if (temp->next != NULL) {
+            write_char(',');
+        }
+
+        temp = temp->next;
     }
 }
 
-static void stack_pop(Stack *stack) {
-    if (stack->size > 0) {
-        stack->size--;
-    }
-}
+// static void stack_pop(Stack *stack) {
+//     if (stack->size > 0) {
+//         stack->size--;
+//     }
+// }
 
 int main() {
 
-    Stack stack;
-    stack_init(&stack);
+    //Stack stack;
+    //stack_init(&stack);
+    struct Node *head;
     int count = 0;
 
-    const char msg[] = "\nInput a string:";
-    write(1, msg, sizeof(msg) - 1);
+    // const char msg[] = "\nInput a string:";
+    // write(1, msg, sizeof(msg) - 1);
 
-    for (;;) {
-        char cmd = read_char();
+    write_string("Input a string:\n");
+
+    char cmd;
+    do {
+        cmd = read_char();
+
 
         if (cmd == 'a') {
-            stack_push(&stack);
+            write_string("Im here a\n");
+            //stack_push(&stack);
             // De to linjer under skal måske ud for loppet og ind i stack_push
-            stack.data[stack.size] = count;
-            stack.size == stack.size + 1;
+            //stack.data[stack.size] = count;
+            insertAtEnd(&head, count);
             count++;
         } else if (cmd == 'b') {
+            write_string("Im here b\n");
             count++;
         } else if (cmd == 'c') {
-            stack_pop(&stack);
+            write_string("Im here c\n");
+            deleteFromEnd(&head);
             count++;
-        } else {
-            write_string("Count: ");
-            write_int(count);
-            write_string("\nCollection: ");
-            for (int i = 0; i < stack.size; ++i) {
-                write_char(stack.data[i]);
-                write_string(", ");
-            }
         }
+
+    } while (cmd == 'a' || cmd == 'b' || cmd == 'c');
+
+        write_string("Count: ");
+        write_int(count);
+        write_string("\nCollection: ");
+        printLinkedList(head);
+        // for (int i = 0; i < stack.size; ++i) {
+        //     write_int(stack.data[i]);
+        //     write_string(", ");
+        // }
+    return 0;
     }
-}
+
 
 
   /*-----------------------------------------------------------------
