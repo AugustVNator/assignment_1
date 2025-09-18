f9834714b8e9482c2c2e4affc77dc293d03bdbf6
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-09-18 13:08:17 +0200

    Remove word

--- test.sh
+++ test.sh
@@ -23,7 +23,7 @@ out=";"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
-# Show we can handle more ints higher than 9
+# Show we can handle ints higher than 9
 in="aaaaaaaaaaaaaaaaaaaaaaa"
 out="0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22;"
 
