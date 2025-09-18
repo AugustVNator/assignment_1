a3c2762618e226e54b2576943e6526680ee2c5e2
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-09-18 13:07:49 +0200

    Change test to show n > 9

--- test.sh
+++ test.sh
@@ -23,7 +23,8 @@ out=";"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
-in="bbbbbat"
-out="5"
+# Show we can handle more ints higher than 9
+in="aaaaaaaaaaaaaaaaaaaaaaa"
+out="0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22;"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
